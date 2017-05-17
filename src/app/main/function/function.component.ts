363aea598264a4b16f492ac9fdee2c7c04c568f2
Toanbn <ngoctoan89@gmail.com>
2017-05-17 22:34:36 +0700

    Lesson 30: Assign permission for role and fix prevent change admin role

--- src/app/main/function/function.component.ts
+++ src/app/main/function/function.component.ts
@@ -13,6 +13,7 @@ import { ModalDirective } from 'ngx-bootstrap/modal';
 })
 export class FunctionComponent implements OnInit {
   @ViewChild('addEditModal') public addEditModal: ModalDirective;
+  @ViewChild('permissionModal') public permissionModal: ModalDirective;
   @ViewChild(TreeComponent)
   private treeFunction: TreeComponent;
 
@@ -21,6 +22,9 @@ export class FunctionComponent implements OnInit {
   public entity: any;
   public editFlag: boolean;
   public filter: string = '';
+  public functionId : string;
+  public _permission: any[];
+
   constructor(private _dataService: DataService,
     private notificationService: NotificationService,
     private utilityService: UtilityService) { }
@@ -28,7 +32,27 @@ export class FunctionComponent implements OnInit {
   ngOnInit() {
     this.search();
   }
+public showPermission(id: any) {
+    this._dataService.get('/api/appRole/getAllPermission?functionId=' + id).subscribe((response: any[]) => {
+      this.functionId = id;
+      this._permission = response;
+      this.permissionModal.show();
+    }, error => this._dataService.handleError(error));
 
+  }
+
+  public savePermission(valid: boolean, _permission: any[]) {
+    if (valid) {
+      var data = {
+        Permissions: this._permission,
+        FunctionId: this.functionId
+      }
+      this._dataService.post('/api/appRole/savePermission', JSON.stringify(data)).subscribe((response: any) => {
+        this.notificationService.printSuccessMessage(response);
+        this.permissionModal.hide();
+      }, error => this._dataService.handleError(error));
+    }
+  }
   //Show add form
   public showAddModal() {
     this.entity = {};
