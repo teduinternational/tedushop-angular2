3a36b5ae550f71f1c0d1ae8bfba32eebc3fa07e4
Toanbn <ngoctoan89@gmail.com>
2017-06-03 13:47:35 +0700

    Lesson 39: Export order to excel

--- src/app/main/order/order-detail/order-detail.component.ts
+++ src/app/main/order/order-detail/order-detail.component.ts
@@ -4,6 +4,8 @@ import { DataService } from '../../../core/services/data.service';
 import { NotificationService } from '../../../core/services/notification.service';
 import { UtilityService } from '../../../core/services/utility.service';
 import { MessageContstants } from '../../../core/common/message.constants';
+import { SystemConstants } from '../../../core/common/system.constants';
+
 import { ModalDirective } from 'ngx-bootstrap/modal';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 
@@ -16,6 +18,8 @@ export class OrderDetailComponent implements OnInit {
   public orderDetails: any[];
   public entity: any;
   public totalAmount: number;
+  public orderId: number;
+  public baseFolder : string = SystemConstants.BASE_API;
   constructor(private utilityService: UtilityService,
     private _dataService: DataService,
     private activatedRoute: ActivatedRoute,
@@ -24,10 +28,10 @@ export class OrderDetailComponent implements OnInit {
 
   ngOnInit() {
     this.activatedRoute.params.subscribe((params: Params) => {
-      let orderId = params['id'];
-      this.loadOrder(params['id']);
+      this.orderId = params['id'];
+      this.loadOrder(this.orderId);
 
-      this.loadOrderDetail(params['id']);
+      this.loadOrderDetail(this.orderId);
     });
 
   }
@@ -36,12 +40,17 @@ export class OrderDetailComponent implements OnInit {
       this.entity = response;
     }, error => this._dataService.handleError(error));
   }
-
+  public exportToExcel() {
+    this._dataService.get('/api/order/exportExcel/' + this.orderId.toString()).subscribe((response: any) => {
+      console.log(response);
+      window.open(this.baseFolder + response.Message);
+    }, error => this._dataService.handleError(error));
+  }
   public loadOrderDetail(id: number) {
     this._dataService.get('/api/order/getalldetails/' + id.toString()).subscribe((response: any[]) => {
       this.orderDetails = response;
       this.totalAmount = 0;
-      for(var item of this.orderDetails){
+      for (var item of this.orderDetails) {
         this.totalAmount = this.totalAmount + (item.Quantity * item.Price);
       }
       console.log(this.totalAmount);
